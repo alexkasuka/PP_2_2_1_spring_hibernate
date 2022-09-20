ac832a40713ab0231ebd8bd5594d690dd51065ba
Alexey Zarubin <alzarubin@gmail.com>
2022-09-20 14:58:18 +0300

    fix OneToOne linkage

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,19 +16,37 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("Alexey", "Zarubin", "zarubin@mail.ru");
+      Car car1 = new Car("Jaguar", 21688225);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+      user1.setCar(car1);
+      car1.setUser(user1);
+
+      User user2 = new User("Irina", "Petrova", "petrova@mail.ru");
+      Car car2 = new Car("Pontiac", 64633225);
+
+      user2.setCar(car2);
+      car2.setUser(user2);
+
+      userService.add(user1);
+      userService.add(user2);
+
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println("Car model = "+user.getCar().getModel());
+//         System.out.println("Car series = "+user.getCar().getSeries());
+//         System.out.println();
+//      }
+
+      Object userByCar1 = userService.getUserByCar("Jaguar", 21688225);
+      System.out.println(userByCar1);
+
+      Object userByCar2 = userService.getUserByCar("Pontiac", 64633225);
+      System.out.println(userByCar2);
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,6 +2,7 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -25,5 +26,14 @@ public class UserDaoImp implements UserDao {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
+
+    @Override
+    public User getUserByCar(String model, int series) {
+        Query<User> query = sessionFactory.getCurrentSession().createQuery("from User where car.model = :model and car.series = :series");
+        query.setParameter("model", model);
+        query.setParameter("series", series);
+
+        return query.getSingleResult();
+    }
 
 }
